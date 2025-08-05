9ca62d33fc2a920281bd469afa3bc240b4d307f5
Herbie <94860953+HerbieJames@users.noreply.github.com>
2025-08-05 14:36:04 +0100

    refactor GlobalAudio

--- main.gd
+++ main.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var latest_ping
 
 func _on_button_pressed() -> void:
-	AudioManager.create_sound("ping")
+	latest_ping = GlobalAudio.create_sound("ping", true)
+
+
+func _on_button_2_pressed() -> void:
+	GlobalAudio.destroy_sound("ping")
+
+
+func _on_button_3_pressed() -> void:
+	if latest_ping: latest_ping.queue_free()
