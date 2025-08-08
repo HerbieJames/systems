1f6ebf2838572c8680bf535d97e55542b60be62c
Herbie <94860953+HerbieJames@users.noreply.github.com>
2025-08-08 16:17:50 +0100

    fix 3rd person camera

--- Scenes/Third Person Camera/third_person_camera.gd
+++ Scenes/Third Person Camera/third_person_camera.gd
@@ -1,14 +1,21 @@
 extends SpringArm3D
 
 
-@export var LOOK_RATE  : int      = 180
-@export var MOUSE_RATE : float    = .25
-@onready var camera    : Camera3D = $Camera3D
-var mouse_input        : Vector2  = Vector2()
+@export var LOOK_RATE  : int             = 180
+@export var MOUSE_RATE : float           = .25
+@onready var camera    : Camera3D        = $"Camera3D"
+@onready var player    : CharacterBody3D = get_parent()
+@onready var diff      : Vector3         = position 
+var mouse_input        : Vector2         = Vector2()
 
 
 func _ready() -> void:
 	spring_length = camera.position.z
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _physics_process(delta: float) -> void:
+	position = player.position + diff
 
 
 func _process(delta: float) -> void:
