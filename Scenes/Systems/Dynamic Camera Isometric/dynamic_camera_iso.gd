5b4da975ee3c07086ebe1d7bad9adc38df55880e
Herbie <94860953+HerbieJames@users.noreply.github.com>
2025-08-18 16:58:32 +0100

    refine DyCamIso

--- Scenes/Systems/Dynamic Camera Isometric/dynamic_camera_iso.gd
+++ Scenes/Systems/Dynamic Camera Isometric/dynamic_camera_iso.gd
@@ -5,18 +5,22 @@ extends Node3D
 @export var target : Node
 @export_group("Zooming", "size")
 ## Zoom camera resolves to
-@export_range(1, 100, 5) var size_target       : float = 20
+@export_range(1, 100, 1) var size_target         : float = 20
 ## Minimum zoom camera can possess
-@export_range(1, 100, 5) var size_max          : float = 40
+@export_range(0.1, 10, 0.1) var size_margin      : float = 5
 @export_group("Easing", "ease")
 ## Distance to which the camera is able to drift away from target
-# @export_range(0.2, 15, 0.2) var ease_move_margin    : float = 10
+@export_range(0.1, 10, 0.1) var ease_move_margin : float = 5
 ## rate at which camera moves in response to target movement
-@export_range(0.2, 10, 0.2) var ease_move_rate : float = 1
+@export_range(0.1, 10, 0.1) var ease_move_rate   : float = 2
 ## rate at which camera zooms in response to target movement
-@export_range(0.2, 10, 0.2) var ease_zoom_rate : float = 1
+@export_range(0.1, 10, 0.1) var ease_zoom_rate   : float = 2
 
 @onready var camera : Camera3D = $Camera3D
+
+
+func _ready() -> void:
+	camera.size = size_target
 
 
 ## Interpolates the position to the target's position
@@ -26,22 +30,21 @@ func ease_position(delta: float) -> Vector3:
 		lerp(position.y, target.position.y, ease_move_rate * delta),
 		lerp(position.z, target.position.z, ease_move_rate * delta)
 	)
-	# position = (Vector3)(
-	# 	clampf(position.x, target.position.x - move_margin, target.position.x + move_margin),
-	# 	clampf(position.y, target.position.y - move_margin, target.position.y + move_margin),
-	# 	clampf(position.z, target.position.z - move_margin, target.position.z + move_margin),
-	# )
+	position = (Vector3)(
+		clampf(position.x, target.position.x - ease_move_margin, target.position.x + ease_move_margin),
+		clampf(position.y, target.position.y - ease_move_margin, target.position.y + ease_move_margin),
+		clampf(position.z, target.position.z - ease_move_margin, target.position.z + ease_move_margin),
+	)
 	return target.position - position
 
 
 ## Adjusts the size of camera according to position from target
 func ease_zoom(position_delta : Vector3, delta: float) -> void:
-	var pos_dif = position_delta.length()
-	camera.size = size_target + (pos_dif * ease_zoom_rate)
-	camera.size = clampf(camera.size, size_target, size_max)
+	var pos_dif : float = position_delta.length() / ease_move_margin
+	camera.size = move_toward(camera.size, size_target + (size_margin * pos_dif), ease_zoom_rate * delta)
 
 
 func _physics_process(delta: float) -> void:
-	var position_delta
+	var position_delta : Vector3
 	position_delta = ease_position(delta)
 	ease_zoom(position_delta, delta)
